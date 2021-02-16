f33eb6bdda6ab700eca62abe7bdd7b3035b76a2a
Violetbeach <ti6419@gmail.com>
2021-02-17 00:47:58 +0900

    first commit

---
+++ src/main/java/com/violetbeach/daengbu/DaengbuApplication.java
@@ -0,0 +1,13 @@
+package com.violetbeach.daengbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaengbuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaengbuApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/violetbeach/daengbu/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.violetbeach.daengbu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String mainPage() {
+		return "index";
+	}
+
+}
