ed20881d6919dafe86cb3d911c871ba09f1d507d
Violetbeach <ti6419@gmail.com>
2021-03-16 23:51:47 +0000

    feat: Add Lucy-xss-filter for XSS attack prevention

---
+++ src/main/java/com/violetbeach/daengbu/config/WebConfiguration.java
@@ -0,0 +1,22 @@
+package com.violetbeach.daengbu.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+	
+    @Bean
+    public FilterRegistrationBean<XssEscapeServletFilter> filterRegistrationBean() {
+        FilterRegistrationBean<XssEscapeServletFilter> filterRegistration = new FilterRegistrationBean<>();
+        filterRegistration.setFilter(new XssEscapeServletFilter());
+        filterRegistration.setOrder(1);
+        filterRegistration.addUrlPatterns("/*");
+        return filterRegistration;
+    }
+    
+}
