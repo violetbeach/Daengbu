af27efd7ae47e715ef3b55ffececaca018c78815
Lee Jaeheon <ti6419@gmail.com>
2021-07-30 20:52:11 +0900

    Fix: Add resource handler to load image in live

--- src/main/java/com/violetbeach/daengbu/config/WebConfiguration.java
+++ src/main/java/com/violetbeach/daengbu/config/WebConfiguration.java
@@ -3,12 +3,19 @@ package com.violetbeach.daengbu.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/img/post/**")
+				.addResourceLocations("file:src/main/resources/static/img/post/");
+	}
 	
     @Bean
     public FilterRegistrationBean<XssEscapeServletFilter> filterRegistrationBean() {
